cabf7b49dc9e14366e20d3f8c3395c10fe65e62c
FKeir <fmkeir@gmail.com>
2020-02-27 17:01:19 +0000

    Create App with board component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template lang="html">
-
+  <div class="main">
+    <h1> CarcaCodeClan </h1>
+    <board :boardState="boardState"/>
+  </div>
 </template>
 
 <script>
+import Board from './components/Board.vue'
+
 export default {
+  name: "app",
+  data(){
+    return {
+      boardState: Array(121).fill("")
+    }
+  },
+  components: {
+    'board': Board
+  }
 }
 </script>
 
---
+++ src/components/Board.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="board-container">
+    <div v-for="(square, index) in boardState">
+      <p>{{index}}</p>
+      <img :src="boardState[index]" alt="Board" v-if="square">
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'board',
+  props: ["boardState"]
+}
+</script>
+
+<style lang="css" scoped>
+.board-container {
+  display: grid;
+  grid-template-columns: repeat(11, 1fr);
+  grid-template-red: repeat(11, 1fr);
+  width: 770px;
+  height: 770px;
+}
+
+.board-container div {
+  border: 1px solid black;
+  transition: transform .2s;
+  background-color: lightgrey;
+}
+
+.board-container div:hover {
+  transform: scale(1.5);
+  z-index: 1;
+}
+
+</style>
