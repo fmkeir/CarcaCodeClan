57b3b58dc6a55c39b4adba4927e6f0f6b7818939
FKeir <fmkeir@gmail.com>
2020-03-04 14:48:52 +0000

    format code

--- server/models/tile.rb
+++ server/models/tile.rb
@@ -1,9 +1,9 @@
-require_relative('../db/sql_runner')
+require_relative '../db/sql_runner'
 
 class Tile
   attr_reader :id, :centre, :has_junction, :has_shield, :image_url
 
-  def initialize(options)
+  def initialize options
     @id = options['id'].to_i() if options['id']
     @sides = [options['side_0'], options['side_1'], options['side_2'], options['side_3']]
     @centre = options['centre']
@@ -12,11 +12,11 @@ class Tile
     @image_url = options['image_url']
   end
 
-  def get_sides()
+  def get_sides
     return @sides[0..-1]
   end
 
-  def save()
+  def save
     sql = "INSERT INTO tiles
     (side_0, side_1, side_2, side_3, centre, has_junction, has_shield, image_url)
     values
@@ -34,10 +34,10 @@ class Tile
 
   def self.all
     sql = "SELECT * FROM tiles"
-    return SqlRunner.run(sql).map{|tile| Tile.new(tile)}
+    return SqlRunner.run(sql).map{|tile| Tile.new tile}
   end
 
-  def self.delete_all()
+  def self.delete_all
     sql = "DELETE FROM tiles"
     SqlRunner.run(sql)
   end
