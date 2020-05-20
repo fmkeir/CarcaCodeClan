53bcfda2597ca8beaf12eb7b036a2e37cae4f243
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-05-20 09:49:20 +0100

    adds addplayer and player display

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,11 +1,33 @@
 <template lang="html">
   <div class="scoreboard">
     <p>Scoreboard</p>
+    <label for="player-name">Player Name:</label>
+    <input type="text" id="player-name" v-model="playerName"/>
+    <button @click="addPlayer">Add Player</button>
+    <div class="players-display">
+      <div v-for="player in players" class="player-details">
+        <input type="number">
+       <h2> {{player}}  </h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Scoreboard',
+  data() {
+    return {
+      players: [],
+      playerName: ""
+    }
+  },
+  methods: {
+    addPlayer: function () {
+      this.players.push(this.playerName)
+      this.playerName = ""
+    }
+  }
 }
 </script>
 
@@ -17,5 +39,24 @@ export default {
   width: 500px;
   height: 100px;
   background-color: lightgrey;
+}
+
+.players-display {
+  display: flex;
+  height: 70px;
+  width: 90%;
+  margin: 0 auto;
+}
+
+.player-details {
+  width: 80px;
+  height: 80px;
+  text-align: center;
+  border: solid black 1px;
+}
+
+.player-details input {
+  width: 40px;
+  font-size: 1.5em;
 }
 </style>
